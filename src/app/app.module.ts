85b3cd2f0be4e4f6cb387342d383430b72bb741c
YoWenqin <wchen15@smith.edu>
2018-01-28 23:41:49 -0500

    basic configurations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatToolbarModule } from '@angular/material';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
 
+import 'hammerjs';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,MatToolbarModule,NoopAnimationsModule,FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
