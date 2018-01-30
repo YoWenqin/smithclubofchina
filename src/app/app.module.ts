718b6655b131f9579846ef510e2d0f80d468b775
YoWenqin <wchen15@smith.edu>
2018-01-30 00:44:27 -0500

    add header footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MatToolbarModule } from '@angular/material';
+import { MatToolbarModule, MatButtonModule,MatGridListModule } from '@angular/material';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
 
 import 'hammerjs';
+import { DirectoryComponent } from './directory/directory.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { ArchivesComponent } from './archives/archives.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DirectoryComponent,
+    ArticlesComponent,
+    ArchivesComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule,MatToolbarModule,NoopAnimationsModule,FlexLayoutModule
+    BrowserModule,MatToolbarModule,NoopAnimationsModule,FlexLayoutModule,MatButtonModule,MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
